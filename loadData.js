7b3204a53b687af7369062a5511e8d7fbe5deb55
Joseph Blumenthal <Joseph.Blumenthal@Medstar.net>
2019-02-28 19:02:12 -0500

    iterating....

--- loadData.js
+++ loadData.js
@@ -1,5 +1,69 @@
 function onLoad() {
 
-    alert("here I am");
+    doFHIR();
 
+}
+
+
+function doFHIR() {
+
+    promiseMe()
+	.then(function(ptData, ptAllergies) {
+
+	    alert("got fhir data");
+
+
+	});
+
+    
+
+}
+
+
+function promiseMe() {
+ 
+    var deferred = $.Deferred();
+
+    //If the API call fails
+    function onError() {
+
+	deferred.reject("It didn't work");
+
+    }
+
+    //If FHIR call was successful 
+    function onReady(smart)  {
+	
+	if (smart.hasOwnProperty('patient')) {
+	    
+	    var patient = smart.patient;
+	    var pt = patient.read();
+	    
+	    
+	    var obv = smart.patient.api.fetchAll({
+		type: 'Observation',
+		query: {}
+	    });
+
+
+	    //if read() fails
+	    $.when(pt, obv).fail(function(patient, observations) {
+		deferred.reject("read() failed");
+	    });
+	    
+	    //if read() succeeds, return the patient resource data
+	    $.when(pt,obv).done(function(patient, observations) {
+		deferred.resolve(patient, observations);
+	    });
+
+	} else 
+	    deferred.resolve("Missing property patient");
+    }
+
+    //Hit FHIR API
+    FHIR.oauth2.ready(onReady, onError);
+    
+    //return the created promise 
+    return deferred.promise();
+    
 }
