c3ae6c892fe4d0dc84dd9a6f23179522c3a4aa37
Joseph Blumenthal <Joseph.Blumenthal@Medstar.net>
2019-03-01 20:15:35 -0500

    added immunizations

--- loadData.js
+++ loadData.js
@@ -8,17 +8,85 @@ function onLoad() {
 function doFHIR() {
 
     promiseMe()
-	.then(function(ptData, ptAllergies) {
+	.then(function(ptData, ptImmun) {
 
-	    alert("got fhir data");
+	    addDemographics(ptData);
 
+	    addImmunizations(ptImmun);
 
 	});
+}
 
+
+function addDemographics(patient) {
+
+    document.getElementById("pSex").innerHTML += patient.gender;
+
+    var fname = '';
+    var lname = '';
+
+    if (typeof patient.name[0] !== 'undefined') {
+    	fname = patient.name[0].given.join(' ');
+    	lname = patient.name[0].family.join(' ');
+    }
+
+    
+    document.getElementById("pName").innerHTML += lname + ', ' + fname;
     
 
 }
 
+
+function  addImmunizations(immunizations) {
+
+    var numImmuns = immunizations.length;
+
+    var root = document.getElementById("ulImmuns");
+
+    for(var i = 0; i < numImmuns; i++) {
+
+	var immunization = immunizations[i];
+
+	console.log(immunization);
+
+	//alert(immunization.date);
+
+	// var adminDate = immunization.date;
+
+	// var temp = moment(adminDate);
+
+	// console.log(temp.format("M/D/YYYY h:m:sa"));
+
+	// var child = createListItem(immunization.vaccineCode.text);
+
+	var child = createListItem(immunization);
+	
+	root.appendChild(child);
+
+    }
+
+}
+
+function createListItem(immunization) {
+
+    var elem = document.createElement("LI");
+
+
+    var vaccine = immunization.vaccineCode.text;
+
+    var givenDtTm = moment(immunization.date).format("M/D/YYYY h:m:sa");
+    
+    // var textNode = document.createTextNode(nodeText);
+
+    var textNode = document.createTextNode(vaccine + " - Given: " + givenDtTm);
+    
+    elem.className = 'immuns';
+
+    elem.appendChild(textNode);
+
+    return elem;
+    
+}
 
 function promiseMe() {
  
@@ -41,19 +109,19 @@ function promiseMe() {
 	    
 	    
 	    var obv = smart.patient.api.fetchAll({
-		type: 'Observation',
+		type: 'Immunization',
 		query: {}
 	    });
 
 
 	    //if read() fails
-	    $.when(pt, obv).fail(function(patient, observations) {
+	    $.when(pt, obv).fail(function(patient, immunizations) {
 		deferred.reject("read() failed");
 	    });
 	    
 	    //if read() succeeds, return the patient resource data
-	    $.when(pt,obv).done(function(patient, observations) {
-		deferred.resolve(patient, observations);
+	    $.when(pt,obv).done(function(patient, immunizations) {
+		deferred.resolve(patient, immunizations);
 	    });
 
 	} else 
